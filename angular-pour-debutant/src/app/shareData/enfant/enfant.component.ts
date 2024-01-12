4720f0ba8fe63e539eed62e8eee8742177c5cbf8
pascaldeveloppeur <pascalyakouyami@gmail.com>
2024-01-12 01:41:07 +0100

    u

---
+++ angular-pour-debutant/src/app/shareData/enfant/enfant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enfant',
+  standalone: true,
+  imports: [],
+  templateUrl: './enfant.component.html',
+  styleUrl: './enfant.component.css'
+})
+export class EnfantComponent {
+
+}
---
+++ angular-pour-debutant/src/app/shareData/parent/parent.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css'
+})
+export class ParentComponent {
+@Input() donnes:string;
+}
